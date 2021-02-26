59a92d4f61c29b875bc6df375810ad1b4377a0de
khan7171 <sbabar950@gmail.com>
2021-02-26 10:38:57 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import Card from './card'
 
 function App() {
 
-    var student = {name: 'Zaheer Ud Din Babar', age: 25, marks: "80%"};
+    var student = { name: 'Zaheer Ud Din Babar', age: 25, marks: "80%" };
     return <div>
         <Hello student={student} />
-        <br/>
-        <br/>
+        <br />
+        <br />
         <Card />
         <div>
-<h1 style={{textAlign: "center"}}>This is evaluated using expression like 100 + 100 = {100+100}</h1>
+            <h1 style={{ textAlign: "center" }}>This is evaluated using expression like 100 + 100 = {100 + 100}</h1>
         </div>
+        <h1 style={{textAlign: "center"}}>This text was added after the configuration of autodeployment from githUb</h1>
     </div>
 }
 
